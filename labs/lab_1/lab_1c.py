8b9f10ce619d58f9c7d77321e9badcfd5514e8ca
sylviey-yao <sylvieyao@gmail.com>
2026-03-26 11:58:58 -0400

    Update lab_1c.py

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -19,12 +19,14 @@ def max_subarray_sum(nums: list[int]) -> int:
     Returns:
         int: The maximum sum of any contiguous subarray.
     """
+    if len(nums) == 0:
+        return 0
 
     max_current = max_global = nums[0]
     
-    for num in nums:
+    for num in nums[1:]:
         max_current = max(num, max_current + num)
-        if max_current < max_global:
+        if max_current > max_global:
             max_global = max_current
             
     return max_global
